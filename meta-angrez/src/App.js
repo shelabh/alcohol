8d395acacdab2d44cd2712d1be777ef829cebcfd
shelabh <68127067+shelabh@users.noreply.github.com>
2022-02-13 02:39:57 +0530

    change

--- meta-angrez/src/App.js
+++ meta-angrez/src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header/header.tsx'
+import Footer from './components/Footer/footer.tsx'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
