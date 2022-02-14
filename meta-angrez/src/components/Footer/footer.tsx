6d2a6f03986f819a17b08c51cbd861feb530679a
shelabh <68127067+shelabh@users.noreply.github.com>
2022-02-14 19:37:24 +0530

    fifth

--- meta-angrez/src/components/Footer/footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = () => (
-      <div > 
-         footer
-      </div>
-  );
-  
-  export default Header;
--- meta-angrez/src/components/Header/header.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = () => (
-      <div > 
-         header
-      </div>
-  );
-  
-  export default Header;
