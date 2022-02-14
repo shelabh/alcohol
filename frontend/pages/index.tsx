ddaeb3aa164d4c031e9f329c099974ec36d945b9
shelabh <68127067+shelabh@users.noreply.github.com>
2022-02-14 23:10:08 +0530

    seventh

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,10 +5,7 @@ import Header from './header/Header'
 export default function Home() {
   return (
     <div >
-      <Head>
-        <title>Alcohol</title>
-        <link rel="icon" href="" />
-      </Head>
+     
       <Header />
     </div>
   );
