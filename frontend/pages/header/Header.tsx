b67a997b7bf501dc6840542fb9f12d4a0f7e8f74
shelabh <68127067+shelabh@users.noreply.github.com>
2022-02-14 23:43:05 +0530

    eigth

--- frontend/pages/header/Header.tsx
+++ frontend/pages/header/Header.tsx
@@ -1,5 +1,21 @@
-function Header() {
-	return <div>Header</div>
-      }
-      
-export default Header
+import React from 'react';
+
+const Header = () => (
+      <div className="flex flex-row"> 
+          <div>
+		Alcohol
+	  </div>
+	  <div>
+		<div>
+			About Us
+		</div>
+	  </div>
+	  <div>
+		<button>
+			Signup
+		</button>
+	  </div>
+      </div>
+  );
+  
+  export default Header;
