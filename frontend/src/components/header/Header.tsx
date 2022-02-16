cac43259dea33132c3a3f4d476e183f8cb0e019d
shelabh <68127067+shelabh@users.noreply.github.com>
2022-02-16 13:07:01 +0530

    16

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 
 const Header = () => (
-      <div className="flex text-white"> 
-          <div className="">
-		Alcohol
-	  </div>
-	  <div className="flex">
-		<a href="#" className="">
-			About Us
-		</a>
-		<a href="#" className="">
-			Order
-		</a>
-		<a href="#" className="">
-			Contact Us
-		</a>
-	  </div>
-	  <div className="">
-		<button>
-			Signup
-		</button>
-	  </div>
+	<div className="w-full h-full">
+		<div className="flex  text-white justify-betweeen"> 
+			<div className="">
+				Alcohol
+			</div>
+			<div className="flex">
+				<a href="#" className="">
+					About Us
+				</a>
+				<a href="#" className="">
+					Order
+				</a>
+				<a href="#" className="">
+					Contact Us
+				</a>
+			</div>
+			<div className="">
+				<button>
+					Signup
+				</button>
+			</div>
+		</div>
       </div>
   );
   
