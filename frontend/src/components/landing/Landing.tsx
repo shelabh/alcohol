1a74cce83aa4b678c69896992086b3b4a7c27e84
shelabh <68127067+shelabh@users.noreply.github.com>
2022-02-17 00:03:52 +0530

    19

--- frontend/src/components/landing/Landing.tsx
+++ frontend/src/components/landing/Landing.tsx
@@ -5,13 +5,14 @@ const Header = () => (
 	<div className="w-full h-full ">
 		<div className="flex  justify-center   items-center w-full h-screen bg-gradient-to-br from-neutral-500 via-neutral-900 to-black  pt-[5%]"> 
 		
-			<div className="text-white pt-11 ">
+			<div className="text-white pt-11  ">
 				<div className="text-9xl  text-right ">
 					ONE
 				</div>
-				<div className=" py-9 w-[30.5%] text-right">
-					A quick way to tell if another round will be fun or a mistake	
-					
+				<div className=" py-9    text-right">
+					<span>
+					 	A quick way to <br />tell if another round <br />will be <span className="text-fun">fun</span> or a <span className="text-mistake">mistake</span>
+					</span>
 				</div>
 				<div className="text-9xl text-right "> 
 					DRINK
